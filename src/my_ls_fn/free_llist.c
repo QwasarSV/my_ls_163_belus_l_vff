aac2036915389c773cfd55256833bb1a6ce6d2f7
cButNotOnly <lorris.belus@icloud.com>
2022-11-03 07:28:51 -0700

    final_test

--- src/my_ls_fn/free_llist.c
+++ src/my_ls_fn/free_llist.c
@@ -1,14 +1,21 @@
 #include "../../include/main_header.h"
 
-void print_and_free_llist(node_t* m_head, bool *flags) {
+void print_and_free_llist(node_t* m_head, my_getopt_t *getopt_ptr) {
 
     node_t *tmp = m_head; 
-
+    int index = 0;
     while (tmp != NULL) {
-        test_print_list(tmp->daughter_head, flags);
+        if(getopt_ptr->count_str > 1) {
+            printf("%s :\n",getopt_ptr->path_arr[index]);
+        }  
+        test_print_list(tmp->daughter_head, getopt_ptr->boll_arr);
         free_node(tmp->daughter_head);
         tmp = tmp->next;
+        index +=1;
+        if(index < getopt_ptr->count_str) {
+            printf("\n");
+        }
     }
-    //free_node(m_head);
+    free_node(m_head);
 
 }
