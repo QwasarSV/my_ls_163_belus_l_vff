21fa07a963bbfdbb8ffaed6172055be2d54e1d5e
lorris belus <belus_l@qwasar.io>
2022-11-04 17:49:44 +0000

    sort_asc fix before fn ptr implementation

--- src/linkedList/sort_asc.c
+++ src/linkedList/sort_asc.c
@@ -9,24 +9,20 @@ void sort_asc(node_t* m_node) {
         while(tmp != NULL) {
             next = tmp;
             while(next != NULL) {
-                
-                if(tmp->st.st_mtim.tv_sec < next->st.st_mtim.tv_sec ) {
-                    m_node->daughter_head = swap(m_node->daughter_head, tmp->value, next->value);        
-                    //printf("1st // swap %s  with %s \n",tmp->path_name, next->path_name);
-                    tmp = next;
-                } else if(tmp->st.st_mtim.tv_sec == next->st.st_mtim.tv_sec ) {
-                    if(tmp->st.st_mtim.tv_nsec < next->st.st_mtim.tv_nsec ) {
+                if(tmp->st.st_mtim.tv_sec == next->st.st_mtim.tv_sec ) {
+                    if (tmp->st.st_mtim.tv_nsec == next->st.st_mtim.tv_nsec){
+                        if(my_strcmp(tmp->path_name, next->path_name) > 0) {
+                            m_node->daughter_head = swap(m_node->daughter_head, tmp->value, next->value);        
+                            tmp = next;
+                        }
+                    } else if (tmp->st.st_mtim.tv_nsec < next->st.st_mtim.tv_nsec){
                         m_node->daughter_head = swap(m_node->daughter_head, tmp->value, next->value);        
-                        //printf("2 nd //swap %s  with %s \n",tmp->path_name, next->path_name);
                         tmp = next;
-                    } 
-                    // if(str_cmp_check(tmp, next) == true && m_node->daughter_head->value != next->value) {
-                    // m_node->daughter_head = swap(m_node->daughter_head, tmp->value, next->value);        
-                    // tmp = next;
-                    // }
-                } 
-                // printf("tmp name : %s time :%li \n",tmp->path_name,tmp->st.st_mtim.tv_nsec);
-                // printf("next name : %s time :%li \n",next->path_name,next->st.st_mtim.tv_nsec);
+                    }
+                } else if(tmp->st.st_mtim.tv_sec < next->st.st_mtim.tv_sec) {
+                    m_node->daughter_head = swap(m_node->daughter_head, tmp->value, next->value);        
+                    tmp = next;
+                }
                 next = next->next;
             }
         tmp = tmp->next;
